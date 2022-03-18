4c592ae41fc1bf0567947babb800d055062bafcc
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-18 08:00:29 +0700

    Print xyz text to canvas element

---
+++ canvas-print-text/script.js
@@ -0,0 +1,6 @@
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+document.body.appendChild(canvas);
+
+context.font = "20px arial";
+context.fillText("xyz", 50, 50);
