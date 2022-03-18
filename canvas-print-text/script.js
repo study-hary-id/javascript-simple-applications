a234b37678e63934f70292c6b10c7227ef5185bd
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-18 09:24:56 +0700

    Print text directyle to html document

--- canvas-print-text/script.js
+++ canvas-print-text/script.js
@@ -1,6 +1,12 @@
+// Print text on canvas element using fillText.
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
 document.body.appendChild(canvas);
 
 context.font = "20px arial";
 context.fillText("xyz", 50, 50);
+
+// Print text directly to html document using write method.
+document.body.style.fontFamily = "arial";
+document.body.style.fontSize = "20px";
+document.write("xyz");
