297a628890d291731857cbb3f5e47b02fb4d1c1d
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-25 03:15:14 +0700

    Update canvas-print-text

--- canvas-print-text/script.js
+++ canvas-print-text/script.js
@@ -1,12 +1,16 @@
 // Print text on canvas element using fillText.
+var canvasText = document.getElementById("canvas-text");
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
-document.body.appendChild(canvas);
+
+canvas.setAttribute("id", "second-canvas");
+canvasText.appendChild(canvas);
 
 context.font = "20px arial";
 context.fillText("xyz", 50, 50);
 
 // Print text directly to html document using write method.
+var htmlText = document.getElementById("html-text");
 document.body.style.fontFamily = "arial";
 document.body.style.fontSize = "20px";
 document.write("xyz");
